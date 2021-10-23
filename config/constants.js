3dd9a1d074ab4a4afde089d690278d647cf06ebe
Catalin Ungureanu <catalinungureanu@gmail.com>
2021-10-23 19:57:21 +0300

    deployed to firebase

--- config/constants.js
+++ config/constants.js
@@ -1,11 +1,11 @@
 export class Constants {
   static openLibraryCover(imageName) {
-    return `http://covers.openlibrary.org/b/id/${imageName}-M.jpg`;
+    return `https://covers.openlibrary.org/b/id/${imageName}-M.jpg`;
   }
 
   static openLibraryLink(bookKey) {
     return `https://openlibrary.org${bookKey}`;
   }
 
-  static booksUrl = 'http://openlibrary.org/search.json?title=the+lord+of+the+rings';
+  static booksUrl = 'https://openlibrary.org/search.json?title=the+lord+of+the+rings';
 }
